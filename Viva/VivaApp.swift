b1f76ba385d7506d9842854aeaa8e72f852bda58
Bruno <clayfetus@gmail.com>
2025-01-08 23:10:58 -0500

    Initial Commit

---
+++ Viva/VivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VivaApp.swift
+//  Viva
+//
+//  Created by Bruno Souto on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct VivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VivaTests/VivaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VivaTests.swift
+//  VivaTests
+//
+//  Created by Bruno Souto on 1/8/25.
+//
+
+import Testing
+@testable import Viva
+
+struct VivaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
